144627ddfcef80748183b2531417c68f7f8fea33
youkiery <youkiery@gmail.com>
2023-09-16 09:24:01 +0700

    chọn ảnh

--- src/app/thanhphan/hinhanh/hinhanh.component.ts
+++ src/app/thanhphan/hinhanh/hinhanh.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ChungService } from 'src/app/dichvu/chung.service';
-import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
-import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Camera, CameraResultType, CameraSource, Photo, ImageOptions } from '@capacitor/camera';
 import { Preferences } from '@capacitor/preferences';
 
 @Component({
@@ -11,20 +10,47 @@ import { Preferences } from '@capacitor/preferences';
 })
 export class HinhanhComponent  implements OnInit {
   @Input() luonganh: string // it, nhieu
+  public base64 = ""
   constructor(
-    public chung: ChungService
+    public chung: ChungService,
   ) { }
 
   ngOnInit() {}
 
   ionViewOnEnter() {}
 
-  public async addNewToGallery() {
+  public async chupanh() {
     // Take a photo
-    const capturedPhoto = await Camera.getPhoto({
+    let camermaOptions: ImageOptions = {
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
-    });
+    }
+
+    Camera.getPhoto(camermaOptions).then((imageData) => {
+      this.base64 = 'data:image/jpeg;base64,' + imageData;
+      // do something
+    }, (err) => {
+      console.log("failed")
+    })
+  }
+
+  public chonanh() {
+    let imageoption = {
+      quality: 100,
+      resultType: CameraResultType.Base64
+    }
+
+    Camera.pickImages(imageoption).then((imageData) => {
+      console.log(imageData);
+      
+      // reader.readAsDataURL(image); 
+      // reader.onloadend = function() {
+      //   console.log(reader.result);
+      // }
+      // do something
+    }, (err) => {
+      console.log("failed")
+    })
   }
 }
