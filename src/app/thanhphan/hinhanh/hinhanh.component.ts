8fda4962d916886e2074d73b7828ca27a289c65a
youkiery <youkiery@gmail.com>
2023-12-30 16:25:55 +0700

    hình ảnh

--- src/app/thanhphan/hinhanh/hinhanh.component.ts
+++ src/app/thanhphan/hinhanh/hinhanh.component.ts
@@ -1,5 +1,5 @@
 import { Component, forwardRef, ViewEncapsulation, Input, Output, EventEmitter, OnInit } from '@angular/core';
-import { Camera, CameraResultType, GalleryPhotos } from '@capacitor/camera';
+import { Camera, CameraResultType, GalleryPhoto } from '@capacitor/camera';
 
 @Component({
   selector: 'app-hinhanh',
@@ -7,41 +7,53 @@ import { Camera, CameraResultType, GalleryPhotos } from '@capacitor/camera';
   styleUrls: ['./hinhanh.component.scss'],
 })
 export class HinhanhComponent implements OnInit {
-  @Input() luonganh: number = 0 // 0: 1 anh, 1: nhieu anh
+  @Input() luonganh: string = "0" // 0: 1 anh, 1: nhieu anh
   @Output() thaydoi = new EventEmitter<any>();
   public hinhanh = []
-  constructor() { }
+  public mokhung = false
+  constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() { }
 
-  ionViewOnEnter() {  }
+  ionViewOnEnter() { }
 
   public async chupanh() {
+    this.mokhung = false
     const anhchup = await Camera.getPhoto({
       quality: 90,
       allowEditing: true,
       resultType: CameraResultType.Base64
     });
     var dulieuanh = "data:image/jpeg;base64," + anhchup.base64String;
-    if (!this.luonganh) this.hinhanh = [dulieuanh]
+    if (this.luonganh == '0') this.hinhanh = [dulieuanh]
     else this.hinhanh.push(dulieuanh)
     this.thaydoi.emit(this.hinhanh)
   }
 
   public chonanh() {
+    this.mokhung = false
     Camera.pickImages({
       quality: 90,
     }).then(file_uri => {
-      this.readAsBase64(file_uri).then((base64: string) => {
-        if (!this.luonganh) this.hinhanh = [base64]
-        else this.hinhanh.push(base64)
-        this.thaydoi.emit(this.hinhanh)
-      })
+      console.log(file_uri);
+      file_uri.photos.forEach(photo_uri => {
+        this.readAsBase64(photo_uri).then((base64: string) => {
+          if (this.luonganh == '0') this.hinhanh = [base64]
+          else this.hinhanh.push(base64)
+          this.thaydoi.emit(this.hinhanh)
+        })
+      });
     }, err => console.log(err));
   }
 
-  private async readAsBase64(cameraPhoto: GalleryPhotos) {
-    const response = await fetch(cameraPhoto.photos[0].webPath!);
+  public xoaanh(thutu: number) {
+    this.hinhanh = this.hinhanh.filter((hinhanh, thutuhinhanh) => {
+      return thutu !== thutuhinhanh
+    })
+  }
+
+  private async readAsBase64(cameraPhoto: GalleryPhoto) {
+    const response = await fetch(cameraPhoto.webPath!);
     const blob = await response.blob();
     return new Promise(resolve => {
       const reader = new FileReader
